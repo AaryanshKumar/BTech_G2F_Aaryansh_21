8e8f257f24682a80d5694762485f321b6018713b
AaryanshKumar <25116cn112@glbitm.ac.in>
2026-02-24 11:37:48 +0530

    Add files via upload

---
+++ AK.Quadratic.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+    float a, b, c, discriminant, root1, root2;
+
+    printf("Enter the Values of a, b and c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    discriminant = b*b - 4*a*c;
+
+    if (discriminant > 0){
+        root1 = (-b + sqrt(discriminant)) / (2*a);
+        root2 = (-b + sqrt(discriminant)) / (2*a);
+        printf("Roots are real and different\n");
+        printf("Root1 = %.2f\nRoot2 = %.2f", root1, root2);
+    }
+    else if (discriminant == 0) {
+        root1 = -b / (2*a);
+        printf("Roots are real and equal\n");
+        printf("Root1 = Root2 = %.2f", root1);
+    }
+    else {
+        printf("Roots are Complex and Imaginary");
+    }
+    return 0;
+}
